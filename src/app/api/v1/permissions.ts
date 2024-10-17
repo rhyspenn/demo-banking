93a3f0af0e76e1d458fa79a46fcd729aba08fba5
Markus Ecker <markus.ecker@gmail.com>
2024-10-17 15:52:33 +0200

    extend demo - permissions etc

---
+++ src/app/api/v1/permissions.ts
@@ -0,0 +1,15 @@
+import { MemberRole } from "@/app/api/v1/data";
+
+export const PERMISSIONS = {
+  ADD_CARD: [MemberRole.Admin, MemberRole.Assistant],
+  ADD_POLICY: [MemberRole.Admin, MemberRole.Assistant],
+  ADD_NOTE: [MemberRole.Admin, MemberRole.Assistant, MemberRole.Member],
+  SHOW_TRANSACTIONS: [
+    MemberRole.Admin,
+    MemberRole.Assistant,
+    MemberRole.Member,
+  ],
+  SET_PIN: [MemberRole.Admin, MemberRole.Assistant, MemberRole.Member],
+  APPROVE_TRANSACTION: [MemberRole.Admin, MemberRole.Assistant],
+  READ_MSA: [MemberRole.Admin],
+};
--- src/app/api/v1/transactions/route.ts
+++ src/app/api/v1/transactions/route.ts
@@ -2,5 +2,5 @@ import { data } from "../data";
 
 // Get all transactions
 export const GET = async () => {
-    return new Response(JSON.stringify(data.transactions), { status: 200 });
+  return new Response(JSON.stringify(data.transactions), { status: 200 });
 };
